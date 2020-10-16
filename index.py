e3c59754fde75359096759afd3a527fb6fcdc8fb
Anil <anillamasg@gmail.com>
2020-10-17 00:17:00 +1100

    Comments

--- index.py
+++ index.py
@@ -48,7 +48,7 @@ allMetrics = ['confirmed', 'recovered', 'deaths']
 def create_dataset(dataset, time_step=1):
     dataX, dataY = [], []
     for i in range(len(dataset) - time_step - 1):
-        a = dataset[i:(i + time_step), 0]  ###i=0, 0,1,2,3-----99   100
+        a = dataset[i:(i + time_step), 0]  ###i=0, 0,1,2,3-----49   50
         dataX.append(a)
         dataY.append(dataset[i + time_step, 0])
     return np.array(dataX), np.array(dataY)
